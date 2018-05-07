475b6aa54c80b1ddffbbc982406f76bb217d3be6
danmcgill6 <danmcgill6@gmail.com>
2018-05-07 13:43:43 -0400

    created cards for folder display

--- src/Components/FolderDisplay.jsx
+++ src/Components/FolderDisplay.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
 
- const FolderDisplay = (foderName, folderDate,) => {
+ const FolderDisplay = ({folders}) => {
+
+     let displayedFolders = folders.map(folder => 
+        <div className="col s12 m4">
+        <div className="card white darken-1">
+          <div className="card-content black-text">
+            <span className="card-title">{folder.name}</span>
+            <p>Created:{folder.createdAt}</p>
+          </div>
+          <div className="card-action">
+            <a href="#">View</a>
+          </div>
+        </div>
+      </div>
+    )
     return ( 
-        <div>
-            <h2>foldername</h2>    
-            <h2>folderDate</h2>
+        <div className="row">
+            {
+                displayedFolders
+            }
         </div>
     );
 }
+
+export default FolderDisplay
--- src/Components/Library.jsx
+++ src/Components/Library.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
+import FolderDisplay from './FolderDisplay'
 
 export default class Library extends React.Component {
-  constructor() {
-   super()
+  constructor(props) {
+      super(props)
+      this.state = {
+          folders:[]
+        }
+  }
+  componentDidMount(){
+    axios.get('http://localhost:8080/api/folders')
+        .then(res => this.setState({folders:res.data}))
   }
 
   render() {
 
     return ( 
-        <div>
-            <h1>Your Library</h1>
+        <div className="folderContainer">
+            <FolderDisplay folders={this.state.folders} />
         </div>
 
     );
